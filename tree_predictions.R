a682bbff0c911021adc8d03db26416cc8280f4f4
AzizAlimov <iamazizalimov@gmail.com>
2022-04-07 23:31:59 -0700

    refactored tree predictions

--- tree_predictions.R
+++ tree_predictions.R
@@ -2,139 +2,114 @@ library(mlbench)
 library(rpart)
 library(rpart.plot)
 library(VGAM)
-library(randomForest)
 library(pROC)
 library(caret)
 library(ggplot2)
+library(quantregForest)
+library(randomForest)
+
 
 options(warn=-1)
 
-
-CategoryPredInterval = function(ProbMatrix,labels) {
-  ncases=nrow(ProbMatrix)
-  pred50=rep(NA,ncases)
-  pred80=rep(NA,ncases)
-  for(i in 1:ncases) {
-    p=ProbMatrix[i,]
-    ip=order(p,decreasing=T)
-    pOrdered=p[ip] # decreasing order
-    labelsOrdered=labels[ip] # decreasing order
-    G=cumsum(pOrdered) # cumulative sum from largest
-    k1=min(which(G>=0.5)) # level1= 0.5
-    k2=min(which(G>=0.8)) # level2= 0.8
-    pred1=labelsOrdered[1:k1]
-    pred2=labelsOrdered[1:k2]
-    pred50[i]=paste(pred1,collapse="")
-    pred80[i]=paste(pred2,collapse="")
+FindUniquePos=function(values,groupValues,tolerance=1.e-5) { 
+  ngroup = length(groupValues) # number of groups (nodes)
+  temp = unique(groupValues)
+  if(length(temp)<ngroup)
+  { cat("Won't work: non-unique group values\n"); return(0); }
+  npred = length(values) # number of cases to bin into a group label
+  group = rep(0,npred) # initialize as group 0
+  for(i in 1:ngroup)
+  { # group[values==groupValues[i]]=i # better to use tolerance
+    igroup = (abs(values-groupValues[i])<tolerance)
+    group[igroup] = i # group label according to position in groupValues
   }
-  list(pred50=pred50, pred80=pred80)
+  if( any(group==0) ) cat("Warning: some values not matched to groupValues\n")
+  return(group)
 }
 
-crossValidationCont = function(df,K,a,nperfmeas=6)
-{ set.seed(123)
-  n = nrow(df)
-  nhold = round(n/K) # size of holdout set 
-  iperm = sample(n)
-  perfmeas = matrix(0,K,nperfmeas)  
-  models = vector(mode="list", length=3)
-  for(k in 1:K)
-  { indices = (((k-1)*nhold+1):(k*nhold))
-  if( k==K ) indices = (((k-1)*nhold+1):n)
-  indices = iperm[indices]
-  train = df[-indices,]
-  holdout = df[indices,]
-  models[[k]] = lm(price~mark_cat+year+mileage_cat+log_vol_engine+fuel+car_type+gdpc,data=train)
-  pred = predict(models[[k]],newdata=holdout,interval="prediction",level=a)
-  RSS = c(crossprod(models[[k]]$residuals))
-  MSE = RSS / length(models[[k]]$residuals)
-  x = intervalScore(pred, holdout$price, a)
-  y= sqrt(MSE)
-  z = summary(models[[k]])$r.squared
-  perfmeas[k,1:4] = x$summary
-  perfmeas[k,5] = y
-  perfmeas[k,6] = z
-  }
-  avgperfmeas = apply(perfmeas,2,mean)
-  list(perfmeasbyfold=perfmeas, avgperfmeas=avgperfmeas, models= models)
+get_quantiles <- function(training, holdout, model, bottom, upper, predictions) {
+  meanpredRegTree=predictions
+  meanByTNode=tapply(training$price, model$where, mean)
+  QLowerByTNode=tapply(training$price, model$where,quantile,prob=bottom)
+  Q50ByTNode=tapply(training$price, model$where,median)
+  QUpperByTNode=tapply(training$price, model$where,quantile,prob=upper)
+  TNodeGroup=FindUniquePos(meanpredRegTree,meanByTNode)
+  ByTNode = cbind(meanByTNode,QLowerByTNode,Q50ByTNode,QUpperByTNode)
+  QLowerpredRegTree=QLowerByTNode[TNodeGroup]
+  QUpperpredRegTree=QUpperByTNode[TNodeGroup]
+  predIntRegTree=cbind(meanpredRegTree,QLowerpredRegTree,QUpperpredRegTree)
+  predIntRegTree
 }
 
-training_test_split <- function(Data) {
-  training_length <- (nrow(Data)%/%3*2)
-  n <- nrow(Data)
-  itrain=sample(n, training_length) #
-  training <- Data[itrain,]
-  holdout <- Data[-itrain,]
-  list(training=training, holdout=holdout)
-}
 
-perform_prediction <- function(model, type, holdout) {
-  outpred=predict(model, type=type, newdata=holdout)
-  t50 <- table(holdout$lettr,predint$pred50)
-  t80 <- table(holdout$lettr,predint$pred80)
+predict_tree <- function(model, training, holdout, prune = FALSE) {
+  rpart.plot(model)
+  predictions = predict(model,newdata=holdout,type="vector")
+  pred50Int=get_quantiles(training, holdout, model, 0.25, 0.75, predictions)
+  pred80Int=get_quantiles(training, holdout, model, 0.1, 0.9, predictions)
   
-  s = sprintf("classification rate for 50 percent prediction interval: %s", round(sum(holdout == predint$pred50)/nrow(holdout), 4))
-  print(s)
-  s = sprintf("classification rate for 80 percent prediction interval: %s", round(sum(holdout == predint$pred80)/nrow(holdout), 4))
-  print(s)
-  print(t50)
-  print(t80)
-  for (i in 1:length(letters)) {
-    class_classification_rate(letters, i, t50, 50)
-  }
-  for (i in 1:length(letters)) {
-    class_classification_rate(letters, i, t80, 80)
-  }
+  IS50=intervalScore(pred50Int,holdout$price,0.5)
+  IS80=intervalScore(pred80Int,holdout$price,0.8)
+  out=rbind(IS50$summary,IS80$summary)
+  out
 }
 
-predict_tree <- function(Data, prune = FALSE) {
-  split <- training_test_split(Data)
-  training <- split$training
-  holdout <- split$holdout
-  RegTree=rpart(lettr~.,data=training, method="class")
-  if (prune) {
-    RegTree<-prune.rpart(RegTree,cp=.05)
-  }
-  
-  perform_prediction(RegTree, "prob", holdout)
-  RegTree
+train_tree <- function(training, holdout) {
+  rpart(price~.,data=training)
 }
 
-predict_forest <- function(Data) {
-  split <- training_test_split(Data)
-  training <- split$training
-  holdout <- split$holdout
-  forest = randomForest(lettr~.,data=training, importance=TRUE, proximity=TRUE)
-  perform_prediction(forest, "prob", holdout)
-  forest
+predict_forest <- function(forest, training, holdout) {
+  predRF = predict(forest, what=c(.1,.25,.5,.75,.9), newdata=holdout[,-1])
+  IS50qRF=intervalScore(predRF[,c(3,2,4)],holdout$price, 0.5)
+  IS80qRF=intervalScore(predRF[,c(3,1,5)],holdout$price, 0.8)
+  outqRF=rbind(IS50qRF$summary,IS80qRF$summary)
+  varImpPlot(forest)
+  outqRF
 }
 
-crossValidationCont = function(df,K,a,nperfmeas=6)
-{ set.seed(123)
+train_forest <- function(training, holdout) {
+  x = data.frame(training[,-1])
+  y = unlist(training[,1], use.names=FALSE)
+  quantregForest(x,y,importance=TRUE)
+}
+
+crossValidationCont = function(df,K,training_func,predict_func,nperfmeas=4) {
+  set.seed(123)
   n = nrow(df)
-  nhold = round(n/K) # size of holdout set 
+  nhold = round(n/K)
   iperm = sample(n)
-  perfmeas = matrix(0,K,nperfmeas)  
+  perfmeas50 = matrix(0, K, nperfmeas)  
+  perfmeas80 = matrix(0, K, nperfmeas)
   models = vector(mode="list", length=3)
-  for(k in 1:K)
-  { indices = (((k-1)*nhold+1):(k*nhold))
-  if( k==K ) indices = (((k-1)*nhold+1):n)
-  indices = iperm[indices]
-  train = df[-indices,]
-  holdout = df[indices,]
-  models[[k]] = lm(price~mark_cat+year+mileage_cat+log_vol_engine+fuel+car_type+gdpc,data=train)
-  pred = predict(models[[k]],newdata=holdout,interval="prediction",level=a)
-  RSS = c(crossprod(models[[k]]$residuals))
-  MSE = RSS / length(models[[k]]$residuals)
-  x = intervalScore(pred, holdout$price, a)
-  y= sqrt(MSE)
-  z = summary(models[[k]])$r.squared
-  perfmeas[k,1:4] = x$summary
-  perfmeas[k,5] = y
-  perfmeas[k,6] = z
+  i = 1
+  while (i <= K) { 
+    indices = (((i-1)*nhold+1):(i*nhold))
+    if( i==K ) indices = (((i-1)*nhold+1):n)
+    indices = iperm[indices]
+    train = df[-indices,]
+    holdout = df[indices,]
+    models[[i]] = training_func(train, holdout)
+    print(sprintf("Fold %d", i))
+    results = predict_func(models[[i]], train, holdout)
+    
+    IS50 = results[1,]
+    IS80 = results[2,]
+    
+    colnames(results)=c("level","avgleng","IS","cover")
+    print(results)
+    
+    perfmeas50[i,1:4] = IS50
+    perfmeas80[i,1:4] = IS80
+    i = i + 1
   }
-  avgperfmeas = apply(perfmeas,2,mean)
-  list(perfmeasbyfold=perfmeas, avgperfmeas=avgperfmeas, models= models)
+  avgperfmeas50 = apply(perfmeas50,2,mean)
+  avgperfmeas80 = apply(perfmeas80,2,mean)
+  list(perfmeas50byfold=perfmeas50, avgperfmeas50=avgperfmeas50,
+       perfmeas80byfold=perfmeas80, avgperfmeas80=avgperfmeas80)
 }
 
-logit = vglm(lettr~., multinomial(), data=Data)
-print(summary(logit))
+source("helpers.R")
+load("data/car_prices_subset_all.RData")
+crossValidationCont(subset_selected, 3, train_tree, predict_tree)
+crossValidationCont(subset_selected, 3, train_forest, predict_forest)
+
